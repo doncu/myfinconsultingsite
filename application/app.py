17c23a272ed375c375697825286e549165b116a4
s.dontsu <s.dontsu@rambler-co.ru>
2018-01-19 21:52:19 +0300

    remove mail_flask

--- application/app.py
+++ application/app.py
@@ -8,7 +8,6 @@ import config
 from application import db
 from application import const
 from application.admin import index
-from flask_mail import Mail
 
 app = Flask(__name__, template_folder=config.TEMPLATE_PATH, static_folder=config.STATIC_PATH)
 app.config.from_object('config')
@@ -23,7 +22,6 @@ babel = Babel(app)
 app.add_template_global(const.MENU, 'MENU')
 app.add_template_global(const.YEAR, name='YEAR')
 
-mail = Mail(app)
 
 
 @app.teardown_request
