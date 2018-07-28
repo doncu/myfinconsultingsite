07f243e34e8bccd51959cfd5d074bbcdf7fa1a57
Ivan Krivosheev <i.krivosheev@cian.ru>
2018-07-28 15:40:24 +0300

    Fix menu

--- myfinconsulting/views.py
+++ myfinconsulting/views.py
@@ -13,12 +13,12 @@ from myfinconsulting.common import email
 
 @app.route('/', endpoint='index')
 def index_view():
-    return render_template('index.html')
+    return render_template('index.html', alias='index')
 
 
 @app.route('/about/', endpoint='about')
 def about_view():
-    return render_template('about.html')
+    return render_template('about.html', alias='about')
 
 
 @app.route('/contacts/', methods=['GET', 'POST'], endpoint='contacts')
@@ -33,7 +33,7 @@ def contacts_view():
         )
         email.send_mail('email.html', **context)
         return redirect(url_for('contacts'))
-    return render_template("contact.html")
+    return render_template('contact.html', alias='contacts')
 
 
 @app.route('/services/', endpoint='services')
@@ -41,24 +41,24 @@ def services_view():
     groups = db.session.query(models.ServiceGroup).options(
         orm.joinedload('services')
     ).order_by(models.ServiceGroup.order).all()
-    return render_template('services.html', groups=groups)
+    return render_template('services.html', groups=groups, alias='services')
 
 
 @app.route('/articles/', endpoint='articles')
 def articles_view():
     articles = db.session.query(models.Article).all()
-    return render_template('articles.html', articles=articles)
+    return render_template('articles.html', articles=articles, alias='articles')
 
 
 @app.route('/article/<oid>', endpoint='article')
 def article_view(oid):
     article = db.session.query(models.Article).filter(models.Article.id == oid).first()
-    return render_template('article.html', article=article)
+    return render_template('article.html', article=article, alias='article')
 
 
-@app.route("/privacy-policy/", endpoint='privacy_policy')
+@app.route('/privacy-policy/', endpoint='privacy_policy')
 def privacy_policy():
-    return render_template('privacy_policy.html')
+    return render_template('privacy_policy.html', alias='privacy_policy')
 
 
 @app.route('/img/<filename>', endpoint='image')
